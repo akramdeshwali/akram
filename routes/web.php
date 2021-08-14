53b70cd00e2af8fc9e0b00649d0dc9f60147f02f
Mohammad Sameer <infosameer2712@gmail.com>
2021-08-14 14:28:13 +0530

    login done

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 
-// Route::get('/', function () {
-//     return view('login');
-// });
+Route::get('/login', function () {
+    return view('login');
+});
 
-Route::view('login', 'login');
+Route::post('login',[UserController::class, 'login']);
+Route::get('/',[ProductController::class, 'index']);
